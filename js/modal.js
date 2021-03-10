0a7ee9facf79a64de6a2a68ed7740f1ee40cd6db
Julia Sinitsia <76127279+JuliSinitsia@users.noreply.github.com>
2021-03-10 03:50:44 +0300

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -5,10 +5,26 @@
       modal: document.querySelector('[data-modal]'),
     };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+    if (refs.openModalBtn && refs.closeModalBtn) {
+      refs.openModalBtn.addEventListener('click', toggleModal);
+      refs.closeModalBtn.addEventListener('click', toggleModal);
+    }
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+  $(function () {
+    $('#nav-mobile-btn').click(function () {
+      if ($(this).attr('data-status') == 'off') {
+        $('.nav-mobile').fadeIn();
+        $('body, html').css('overflow-y','hidden');
+        $(this).attr('data-status', "on");
+      } else {
+        $('.nav-mobile').fadeOut();
+        $('body, html').css('overflow-y','auto');
+        $(this).attr('data-status', "off");
+      }
+    });
+  });
